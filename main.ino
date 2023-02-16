cce401b205e54e18aa734165232544dd6cb758cb
bullfroges <124937576+bullfroges@users.noreply.github.com>
2023-02-16 10:55:25 -0800

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,7 +1,41 @@
+// include all libraries
+
+// set variables for all hardwares
+
+//setup some other helpful variable
+int state = 0; //state == 0 is line following state == 1 is grabbing
+
+
+//initialization
 void setup(){
-  
+
+	//set pin
+
+	// Initialize pixy
+
+	//check claw position and set it to open.
+
+	//check camera position and set it to down
+
 }
 
 void loop(){
+	if(state){
+		//switch to color identification library
 
+		//set camera position to up
+
+		//continue moving until close to the object. And close claw
+		
+	}else{
+
+		//check for vectors, coordinates, and interactions
+
+		//case1 no new vector (straight)
+
+		//case 2 new vector. (calculate movement using coordinates)
+
+		//case 3 saw at the end of line && set state to 1
+	}
+	
 }
